5c90fc8dff06a0c94263fe52853c00f60813a6c8
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-14 10:27:16 +0200

    H

--- run/script.js
+++ run/script.js
@@ -1,7 +1,6 @@
 var u = window.location.href.split("?").pop();
 const content_var = decodeURI(u);
 console.log(content_var);
-const content = document.getElementById("textarea-id").value;
     
 const blob= new Blob([content_var], {
     type:'text/html'
