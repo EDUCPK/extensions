ff6888d607a79cd0349e9e65b3cebb0e069f19ad
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-14 10:58:39 +0200

    TEST

--- run/script.js
+++ run/script.js
@@ -1,11 +1,29 @@
-var u = window.location.href.split("?").pop();
-const content_var = decodeURI(u);
-console.log(content_var);
-    
-const blob= new Blob([content_var], {
-    type:'text/html'
+window.addEventListener('message', (event) =>{
+    if(event.origin !=="https://EDUCPK.github.io/extensions/vscode/extensions/startup"){
+        var u = window.location.href.split("?").pop();
+        const content_var = decodeURI(u);
+        console.log(content_var);
+            
+        const blob= new Blob([content_var], {
+            type:'text/html'
 
-});
+        });
 
-const fileurl = URL.createObjectURL(blob);
-window.location.href = fileurl
+        const fileurl = URL.createObjectURL(blob);
+        window.location.href = fileurl
+    }else{
+        window.location.href = "https://EDUCPK.github.io/extensions/run?<p>404</p>"
+        var u = window.location.href.split("?").pop();
+        const content_var = decodeURI(u);
+        console.log(content_var);
+            
+        const blob= new Blob([content_var], {
+            type:'text/html'
+
+        });
+
+        const fileurl = URL.createObjectURL(blob);
+        window.location.href = fileurl
+    }
+})
+
