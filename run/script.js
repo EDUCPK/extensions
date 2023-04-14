c5d197a5a0d654ba92cd3542dc74826a71112a43
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-14 10:17:39 +0200

    Test

--- run/script.js
+++ run/script.js
@@ -1,4 +1,4 @@
 var u = window.location.href.split("?").pop();
 const content = decodeURI(u);
 console.log(content);
-document.getElementById("cont").innerHTML = content
+document.getElementById("cont-box").innerHTML = content
