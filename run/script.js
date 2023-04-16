c12b0219cc03015b68975671de2288d385306d32
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-16 09:51:36 +0200

    Update script.js

--- run/script.js
+++ run/script.js
@@ -7,31 +7,4 @@ function run(){
     const fileurl = URL.createObjectURL(blob);
     window.location.href = fileurl
 }
-// Import required libraries
-const express = require('express');
-const bodyParser = require('body-parser'); // Middleware for parsing request body
-const axios = require('axios'); // Axios for making HTTP requests
-
-// Create an Express app
-const app = express();
-const port = 3000; // Specify the port for the server to listen on
-
-// Middleware for parsing request body
-app.use(bodyParser.json());
-
-// Define an endpoint to handle POST requests
-app.post('/api/endpoint', (req, res) => {
-  // Access the request payload using req.body
-  const message = req.body;
-
-  // Process the data as needed
-  console.log('Message received:', message);
-
-  // Send a response back to the client
-  res.status(200).json({ success: true, message: 'Message received successfully' });
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+run()
